7277f9081a67da8a5bcded75253f87de6d91e0ef
Tsundere Chen <tsundere@kisekinoashita.org>
2015-07-31 13:13:22 +0800

    First Commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+            $('#fullpage').fullpage({
+                navigation: true,
+                navigationPosition: 'right',
+                sectionsColor: ['#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFC107'],
+                anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage'],
+                menu: '#menu',
+                css3: true,
+                scrollingSpeed: 800,
+                scrollOverflow:true,
+                keyboardScrolling: true,
+                animateAnchor: true,
+                recordHistory: true,
+            });
+        });
